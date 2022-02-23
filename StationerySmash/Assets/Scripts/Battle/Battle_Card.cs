d425c0d95323e4fb74fadb9c163ca91b2d9c85ec
moonkanghyuck <moonkanghyuck@gmail.com>
2022-02-23 14:16:11 +0900

    02/22_카드 스크립트 틀 제작

--- StationerySmash/Assets/Scripts/Battle/Battle_Card.cs
+++ StationerySmash/Assets/Scripts/Battle/Battle_Card.cs
@@ -8,5 +8,22 @@ public class Battle_Card : BattleCommand
 
     public Battle_Card(BattleManager battleManager) : base(battleManager) { }
 
+    /// <summary>
+    /// 최대 장수까지 카드를 뽑는다
+    /// </summary>
+    public void Add_AllCard()
+    {
+        for(int i = 0; i < max_Card; i++)
+        {
+            Add_OneCard();
+        }
+    }
 
+    /// <summary>
+    /// 카드 한장을 뽑는다
+    /// </summary>
+    public void Add_OneCard()
+    {
+
+    }
 }
---
+++ StationerySmash/Assets/Scripts/Battle/CardMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardMove : MonoBehaviour
+{
+    [SerializeField]
+    private Image card_Background;
+    [SerializeField]
+    private Image card_UnitImage;
+    [SerializeField]
+    private TextMeshProUGUI card_UnitCost;
+    [SerializeField]
+    private Image card_Grade;
+    [SerializeField]
+    private TextMeshProUGUI card_Name;
+}
