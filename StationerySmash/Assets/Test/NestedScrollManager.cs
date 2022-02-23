f09b6b3c41464d2e4480a2cf61b8cae7871648ef
NYAHA_1003 <hsk5stars@gmail.com>
2022-02-23 19:03:12 +0900

    0223_Scroll
    스크롤뷰 기초 틀

---
+++ StationerySmash/Assets/Test/NestedScrollManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class NestedScrollManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Scrollbar scrollbar;
+
+    const int SIZE = 3;
+    float[] pos = new float[SIZE];
+    float distance, curPos, targetPos;
+    bool isDrag;
+    int targetIndex;
+
+
+    void Start()
+    {
+        distance = 1f / (SIZE - 1);
+        for(int i = 0; i < SIZE; i++) pos[i] = distance * i;
+    }
+
+    float SetPos()
+    {
+        for (int i = 0; i < SIZE; i++)
+            if (scrollbar.value < pos[i] + distance * 0.5f && scrollbar.value > pos[i] - distance * 0.5f)
+            {
+                targetPos = i;
+                return pos[i];
+            }
+        return 0;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) => curPos = SetPos();
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        isDrag = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isDrag = false;
+        targetPos = SetPos();
+
+        if (curPos == targetPos)
+        {
+            print(eventData.delta.x);
+
+            if(eventData.delta.y > 18 && curPos - distance >= 0)
+            {
+                --targetIndex;
+                targetPos = curPos - distance;
+            }
+
+            else if(eventData.delta.y < -18 && curPos + distance <= 1.01f)
+            {
+                ++targetIndex;
+                targetPos = curPos + distance;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(!isDrag) scrollbar.value = Mathf.Lerp(scrollbar.value, targetPos, 0.1f);
+    }
+}
